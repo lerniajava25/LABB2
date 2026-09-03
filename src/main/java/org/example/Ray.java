192eaa61902381b77c2a934749589596bf39fe8b
osama younes <229142742+ossieX@users.noreply.github.com>
2026-09-03 19:55:38 +0200

    Added Ray class

---
+++ src/main/java/org/example/Ray.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Ray {
+    public Vector3D origin;
+    public Vector3D direction;
+
+    public Ray(Vector3D origin, Vector3D direction) {
+        this.origin = origin;
+        this.direction = direction;
+    }
+}
