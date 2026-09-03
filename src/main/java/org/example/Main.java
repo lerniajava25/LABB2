aeced3cd3cf5bed99018a078d9b802fa13204754
osama younes <229142742+ossieX@users.noreply.github.com>
2026-09-03 22:15:17 +0200

    implemnted colors and added renderar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,2 +1,23 @@
-void main() {
-    IO.println("hello world");
+package org.example;
+
+import org.example.shapes.Sphere;
+import org.example.shapes.Triangle;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scene scene = new Scene();
+
+        Sphere sphere = new Sphere(new Vector3D(0, 0, 5), 1);
+        Triangle triangle = new Triangle(
+                new Vector3D(-2, -1, 4),
+                new Vector3D(2, -1, 4),
+                new Vector3D(0, 2, 4)
+        );
+
+        scene.addShape(sphere);
+        scene.addShape(triangle);
+
+        Renderer renderer = new Renderer(scene);
+        renderer.render("image.png");
+    }
+}
